ae69eb9eec535f0b96db58fa29fe5f5f94848e71
sofia <sofiferro89@gmail.com>
2024-11-27 19:00:50 -0300

    changes in page text

--- app/sobre-este-proyecto/page.tsx
+++ app/sobre-este-proyecto/page.tsx
@@ -31,23 +31,33 @@ export default function ContactPage() {
             <div className={`max-w-4xl mx-auto mb-16 ${styles.contentAnimation}`}>
               <div className="bg-white/5 backdrop-blur-sm rounded-lg p-8 shadow-xl">
                 <div className="space-y-6 text-gray-300 text-lg leading-relaxed">
+{/*                   <p>
+                    La Comuna 3, que abarca los barrios de <b>Balvanera</b> y San Cristóbal, es la tercera con mayor
+                    población migrante en la Ciudad de Buenos Aires, después del Centro y la zona Sur. Según datos
+                    del Instituto Nacional de Estadística y Censos (INDEC), en ambos barrios residen 39.266 migrantes,
+                    una cifra publicada en el marco del <a href="https://censo.gob.ar/index.php/datos_definitivos_caba/" className="underline">
+                      Censo Nacional 2022.
+                    </a>
+                  </p> */}
                   <p>
-                    El proyecto <b>{globalData.projectName}</b> es un archivo interactivo que explora las prácticas
-                    culinarias del barrio de Balvanera, con un enfoque en la apropiación, dispersión y
-                    transmisión de ingredientes y sabores tradicionales, derivados de los procesos
-                    migratorios que fueron moldeando su identidad gastronómica.
+                    <b>{globalData.projectName}</b> funciona como un archivo vivo que explora las 
+                    prácticas culinarias de las comunidades migrantes del barrio de <b>Balvanera</b>, con un enfoque en la 
+                    apropiación, dispersión y transmisión de tradiciones que dieron forma a su identidad 
+                    gastronómica. El proyecto se materializa en una mesa interactiva que recorre los lazos 
+                    entre el barrio y sus diversas comunidades, invitando a reflexionar sobre las relaciones 
+                    culturales que nutren este espacio urbano.
                   </p>
                   <p>
-                    La información recopilada está organizada por <b>nodos</b>, cada uno representando a una
-                    comunidad inmigrante del barrio. Al tapear el botón <i>Imprimir</i>, los visitantes podrán llevar consigo un fragmento del
-                    archivo en formato físico. Este gesto simboliza la continuidad de la dispersión de
-                    ingredientes, sabores y relatos, permitiendo que cada persona contribuya a la transmisión
-                    y resignificación de las prácticas culinarias exploradas en el proyecto.
+                    La información recopilada está organizada en <b>nodos</b>, cada uno representando a una
+                    comunidad del barrio. Al tocar el botón <i>Imprimir</i>, los visitantes podrán llevarse un fragmento del
+                    archivo en formato físico. Este gesto simboliza la continuidad en la difusión de
+                    ingredientes, sabores y relatos, permitiendo que cada persona contribuya a la 
+                    resignificación de las prácticas culinarias exploradas en este proyecto.
                   </p>
                 </div>
               </div>
             </div>
-            <div className={`max-w-4xl mx-auto mb-16 ${styles.contentAnimation}`} style={{animationDelay: '0.2s'}}>
+            <div className={`max-w-4xl mx-auto mb-16 ${styles.contentAnimation}`} style={{ animationDelay: '0.2s' }}>
               <div className="bg-white/5 backdrop-blur-sm rounded-lg p-8 shadow-xl">
                 <div className="space-y-6 text-gray-300 text-lg leading-relaxed">
                   <p><b>
